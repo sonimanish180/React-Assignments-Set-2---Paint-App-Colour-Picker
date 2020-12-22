83fab35ca1548373da2f21f02e9a13c71c964dae
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-12-23 02:35:25 +0530

    changes

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React, { useState } from "react";
 
+export default function Selection(props) {
+  let [background, setBackground] = React.useState("");
+
+  return (
+    <>
+      <div style={background} className="fix-box" onClick={props.applyColor}>
+        <h2 className="subheading">Selection</h2>
+      </div>
+    </>
+  );
+}
